773fadf63db9474dcb1b038094c0305b3f1025d6
Jonathan Burns <jburns@bcsops.com>
2018-06-25 14:17:03 -0400

    initial commit

---
+++ AIProject/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AIProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Worth $1B";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "For questions about insurance, call Art.";
+
+            return View();
+        }
+    }
+}

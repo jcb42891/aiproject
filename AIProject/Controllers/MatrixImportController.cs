41558a2a1213ca9f7baafff39d71ede5b075e1dd
Jon Burns <jcb42891@gmail.com>
2018-06-28 12:46:36 -0400

    Matrix Import page added
    Added the page and a very simple file upload form to save a file to the server.

---
+++ AIProject/Controllers/MatrixImportController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AIProject.Controllers
+{
+    public class MatrixImportController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase aiMatrixFU)
+        {
+            if (aiMatrixFU.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(aiMatrixFU.FileName);
+                var path = Path.Combine(Server.MapPath("~/App_Data/Uploads"), fileName);
+                aiMatrixFU.SaveAs(path);
+            }
+            // test
+            return View();
+        }
+    }
+}
